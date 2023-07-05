d5e804a338495eaf79c9dab583faca695852046a
Imad1assouli <assouli32@gmail.com>
2023-07-05 13:41:31 +0100

    add Attribute to my class

--- MyFirstFile.java
+++ MyFirstFile.java
@@ -1,2 +1,37 @@
+import java.util.Date;
+
 public class MyFirstFile {
+    private String nom;
+    private String prenom;
+    private Date date;
+
+    public MyFirstFile(String nom, String prenom, Date date) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.date = date;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
